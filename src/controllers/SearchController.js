0231cc2fa8410ad02e3986bac8c33c5f606ca54c
Jakub <lemiszewski@gmx.com>
2020-02-15 19:01:08 +0100

    [WIP] Search controller basic search

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -6,26 +6,34 @@ const { ErrorHandlers } = require("../utilities");
 const SearchController = {
     async getQuery(req, res) {
         try {
-            const { fn, ln, cp } = req.query;
+            const set = {};
 
-            const query = {
-                $or: []
-            };
+            if (req.query.fn) {
+                set["firstname"] = req.query.fn;
+            }
+            if (req.query.ln) {
+                set["surname"] = req.query.ln;
+            }
+            if (req.query.cp) {
+                set["experience.company"] = req.query.cp;
+            }
 
-            if (fn) query.$or.push({ firstname: fn });
-            if (ln) query.$or.push({ surname: ln });
-            if (cp) query.$or.push({ "experience.company": cp });
-
-            const searchResult = Profile.find(query, docs => {
-                return docs
-            });
-
-            if ((await searchResult).length === 0)
+            if (Object.keys(set).length != 0) {
+                const searchResult = await Profile.find(set, (err, doc) => {
+                    return doc;
+                });
+                if ((await searchResult).length === 0)
+                    throw new ErrorHandlers.ErrorHandler(
+                        404,
+                        "Query do not provided any result"
+                    );
+                res.status(200).json(searchResult);
+            } else {
                 throw new ErrorHandlers.ErrorHandler(
                     404,
                     "Query do not provided any result"
                 );
-            res.status(200).json(searchResult);
+            }
         } catch (err) {
             res.status(500).json({ message: err.message });
         }
