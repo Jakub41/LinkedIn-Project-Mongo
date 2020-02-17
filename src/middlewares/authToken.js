38864188fe4cdd48f91112bc9f0deab119445e84
Jakub <lemiszewski@gmx.com>
2020-02-17 23:41:18 +0100

    AuthToken fix

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -9,43 +9,29 @@ const jwt = require("jsonwebtoken");
 
 // Access token header
 const accessToken = async (req, res, next) => {
-    // Token pass in header as Bearer authorization
-    if (req.headers["authorization"]) {
-        // Token from header Bearer in front
-        // Getting off of it
-        const accessToken = req.headers["authorization"];
-        const token = accessToken.split(" ")[1];
+    try {
+        if (req.headers["authorization"]) {
+            // Token from header Bearer in front
+            // Getting off of it
+            const accessToken = req.headers["authorization"];
+            const token = accessToken.split(" ");
+            const tk = token[1];
 
-        // User and expiration
-        let userId = "";
-        let exp = "";
+            // Token verification with user provided
+            const { userId } = await jwt.verify(tk, Config.jwt.secret);
 
-        // Token verification with user provided
-        try {
-            const d = await jwt.verify(token, Config.jwt.secret);
-
-            userId = d.userId;
-            exp = d.exp;
-            // Check if token has expired
-            if (exp < Date.now().valueOf() / 1000) {
-                throw new ErrorHandlers.ErrorHandler(
-                    401,
-                    "JWT token has expired, please login to obtain a new one"
-                );
-            }
             // Logged in user find
-            res.locals.loggedInUser = await User.findById(userId).populate(
-                "profile"
-            );
-            res.locals.accessToken = token;
+            res.locals.loggedInUser = await User.findById(userId);
+            // Token logged user
+            res.locals.accessToken = tk;
             next();
-        } catch (err) {
-            console.log(err);
-            return res.status(401).json({ msg: "Token is not valid" });
+        } else {
+            next();
         }
-    } else {
-        next();
+    } catch (err) {
+        next(err);
     }
+    // Token pass in header as Bearer authorization
 };
 
 module.exports = accessToken;
--- src/routes/Search.js
+++ src/routes/Search.js
@@ -2,10 +2,12 @@
 const express = require("express");
 // Controller
 const { SearchCtrl } = require("../controllers");
+// Auth middleware
+const { Auth } = require("../middlewares");
 // Router
 const router = express.Router();
 
 // Post CRUD
-router.get("/", SearchCtrl.getQuery);
+router.get("/", Auth.allowIfLoggedin, SearchCtrl.getQuery);
 
 module.exports = router;
